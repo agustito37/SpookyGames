bb3800bb13295cfe894e6c6e395e53cbea0bc6f0
Federico Machado <federico@iugo.com.uy>
2022-11-08 00:34:40 -0300

    Linked Generator with Scene

--- Assets/Scripts/Maze/MazeExample.cs
+++ Assets/Scripts/Maze/MazeExample.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 public class MazeExample : MonoBehaviour
 {
+
+    public bool FullRandom = false;
+    public int RandomSeed = 12345;
+    public GameObject Floor = null;
+    public GameObject Wall = null;
+    public GameObject Pillar = null;
+
+    public int Rows = 5;
+    public int Columns = 5;
+    public float CellWidth = 5;
+    public float CellHeight = 5;
+    public bool AddGaps = true;
+    public GameObject GoalPrefab = null;
+    public GameObject EndPrefab = null;
+    public GameObject Character = null;
+
+    public GameObject collectible = null;
     // Start is called before the first frame update
     void Start()
     {
-        MazeModel m = MazeFactory.Instance.generateMaze();        
+        MazeModel m = MazeFactory.Instance.generateMaze(Rows, Columns, 10, 10);
         string tmp = "";
-        for (var i = 0; i <  m.matrix.Count; i++)
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
         {
-            for (var j = 0; j < m.matrix.Count; j++)
+            player = Character;
+        }
+
+        for (var column = 0; column < m.matrix.Count; column++)
+        {
+            for (var row = 0; row < m.matrix.Count; row++)
             {
-                tmp += m.matrix[i][j] + " ";
+                float x = column * (CellWidth + (AddGaps ? .2f : 0));
+                float z = row * (CellHeight + (AddGaps ? .2f : 0));
+                GameObject gameObject;
+
+                gameObject = Instantiate(Floor, new Vector3(x, 0, z), Quaternion.Euler(0, 0, 0));
+                gameObject.name = "Floor - " + (new Coordinates(column, row)).ToString();
+                gameObject.transform.parent = transform;
+
+
+
+                if (m.matrix[column][row] == MazeTiles.ITEM)
+                {
+
+                    gameObject = Instantiate(collectible, new Vector3(x, 0, z), Quaternion.Euler(0, 0, 0));
+                    gameObject.name = "Item - " + (new Coordinates(column, row)).ToString();
+                    gameObject.transform.parent = transform;
+                }
+
+                if (m.matrix[column][row] == MazeTiles.START)
+                {
+                    player.transform.position = new Vector3(x, 0, z);
+                }
+
+                if (m.matrix[column][row] == MazeTiles.END)
+                {
+                    gameObject = Instantiate(EndPrefab, new Vector3(x, 0, z), Quaternion.Euler(0, 0, 0));
+                    gameObject.name = "End - " + (new Coordinates(column, row)).ToString();
+                    gameObject.transform.parent = transform;
+                }
+
+
+                if (m.matrix[column][row] == MazeTiles.WALLS)
+                {
+
+                    GameObject parentObject = new GameObject();
+                    parentObject.name = "Set of Walls 1- " + (new Coordinates(column, row)).ToString();
+                    parentObject.transform.parent = transform;
+
+                    for (int k = 0; k < 4; k++)
+                    {
+                        gameObject = Instantiate(Wall, new Vector3(x - k + CellWidth / 2, 0, z) + Wall.transform.position, Quaternion.Euler(0, 90, 0));
+                        gameObject.name = "Wall " + (k + 1) + " " + (new Coordinates(column, row)).ToString();
+                        gameObject.transform.parent = parentObject.transform;
+                    }
+
+                }
+
+
             }
             Debug.Log(tmp);
             tmp = "";
@@ -23,6 +95,6 @@ public class MazeExample : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
